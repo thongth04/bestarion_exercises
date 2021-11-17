0a570d03dd424cd9189b7df790391b55ce9c5bfd
thongth04 <thongth0497@gmail.com>
2021-11-17 21:37:01 +0700

    [EX1511] Add product model validations

--- ex1511/app/models/product.rb
+++ ex1511/app/models/product.rb
@@ -1,7 +1,25 @@
 class Product < ApplicationRecord
 
+  validates :sku, presence: true
+  validates :title, presence: true, length: {minimum:10, maximum:40}
+  validates :price, presence: true
+  validates :quantity, presence: true
+
+
+
   # def find_by_name(name)
   #   products.select { |product| name == product.name }
+  # end
+
+  # def self.fill_out_attr
+  #   products = Product.all
+  #   i = 0
+  #   for product in products
+  #     product[:sku] = "SP1000" + i.to_s
+  #     product[:quantity] = 40
+  #     product.save
+  #     i += 1
+  #   end
   # end
 
 end
