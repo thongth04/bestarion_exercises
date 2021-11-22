899698115d098746f353241a5400ad248087c513
thongth04 <thongth0497@gmail.com>
2021-11-22 10:22:59 +0700

    [EX2211] Add exercise 2211

---
+++ ex2211/main.rb
@@ -0,0 +1,26 @@
+require 'byebug'
+=begin
+Viết chương trình trong đó có hàm nhận input đầu vào là một mảng không rỗng và các phần tử trong mảng
+không trùng nhau (có thể bao gồm số âm), và một con số đại diện cho tổng (target_sum).
+Nếu 2 số trong một mảng có tổng bằng với target_sum, in ra mảng với 2 số đó. Nếu không có in ra mảng rỗng
+Lưu ý: chỉ sử dụng 1 vòng lặp for hoặc while. Chỉ cần in ra 1 mảng đầu tiên tìm thấy
+Vd: input là mảng 
+  numbers = [3, 5, -4, 8, 11, 1, -1, 6]
+  target_sum = 10 
+=> output sẽ là [11, -1]
+=end
+
+numbers = [3, 5, -4, 8, 11, 1, -1, 6]
+target_sum = 10
+res = []
+for num in numbers
+  pair_with_num = (numbers - [num]).select{ |x| x + num == target_sum }
+
+  if pair_with_num[0]
+    res = [num, pair_with_num[0]]
+    break
+  else
+    next
+  end
+end
+p res
