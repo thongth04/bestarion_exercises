e771d2db52418293a57b5bd3f1fe31cf622aefad
thongth04 <thongth0497@gmail.com>
2021-11-30 21:33:23 +0700

    [EX3011] Add secure password and signup form for the users

--- ex3011/app/models/user.rb
+++ ex3011/app/models/user.rb
@@ -10,4 +10,7 @@ class User < ApplicationRecord
                   uniqueness: { case_sensitive: false },
                   length: { maximum: 105 },
                   format: { with: VALID_EMAIL_REGEX }
+
+  has_secure_password
+  
 end
