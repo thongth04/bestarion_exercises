ee87a1bae381b80eaf2b2f89f9afb2610b749af3
thongth04 <thongth0497@gmail.com>
2021-12-23 16:26:12 +0700

    [EX3011] Implement CRUD functions for the users

--- ex3011/app/controllers/users_controller.rb
+++ ex3011/app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:edit, :update, :show, :destroy]
 
   def index
-    @users = User.all.inspect
+    @users = User.all
   end
 
   def show
-    @user = User.find(params[:id]).inspect   
+    @user.inspect
   end
   
   def new
@@ -22,9 +23,29 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Your account was updated successfully!"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:notice] = "Account was successfully deleted!"
+    redirect_to users_path
+  end
+
   private
     def user_params
       params.require(:user).permit(:username, :email, :password, :birthday, :phone)
     end
-    
+    def find_user
+      @user = User.find(params[:id])
+    end
 end
