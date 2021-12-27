d48a19a4ac641373282084bc6874eba798e88018
thongth04 <thongth0497@gmail.com>
2021-12-27 22:23:58 +0700

    [EX3011] Implement login, logout sessions

--- ex3011/app/controllers/users_controller.rb
+++ ex3011/app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "Signed up successfully!"
+      flash[:notice] = "Your account was created successfully!"
       redirect_to @user
     else
       render :new
--- ex3011/config/routes.rb
+++ ex3011/config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   get 'pages/home'
   root 'pages#home'
   
-  get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  
+  get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
 end
